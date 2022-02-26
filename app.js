076a43003cc6d711dff7072b1945e66984acb027
laihuyha <laihuyha@gmail.com>
2022-02-26 10:21:38 +0700

    config mongodb

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// import mongoose
+const mongoose = require('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+
+//db connection
+mongoose.connect(
+        process.env.MONGO_URI, { useNewUrlParser: true }
+    )
+    .then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
