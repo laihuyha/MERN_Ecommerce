f8822cb6906ba50ee07a0f582b414d7d5ec79f96
Huy Ha <laihuyha@gmail.com>
2022-03-07 04:01:39 +0700

    Section 3 - Lesson 14

--- models/user.js
+++ models/user.js
@@ -33,3 +33,29 @@ const userSchema = new mongoose.Schema({
         default: []
     }
 }, { timestamp: true });
+
+//virtual field
+userSchema.virtual('password')
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    });
+.get(function() {
+    return this._password;
+});
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
