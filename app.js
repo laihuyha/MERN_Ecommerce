3f329383b15fb22f273d8a5d9339c74eb71ec3fa
laihuyha <laihuyha@gmail.com>
2022-03-06 13:38:17 +0700

    fix something

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
-//test
+
+//app
+const app = express();
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB Connected')
+}).catch(err => {
+    console.log(`DB connection error: ${err.message}`)
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -11,20 +23,4 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
-
-// import mongoose
-const mongoose = require('mongoose');
-// load env variables
-const dotenv = require('dotenv');
-dotenv.config()
-
-//db connection
-mongoose.connect(
-        process.env.MONGO_URI, { useNewUrlParser: true }
-    )
-    .then(() => console.log('DB Connected'))
-
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`)
 });
