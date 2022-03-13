895d1efc51e6b7fae9d7dad99f7fa41bf5b97b93
HuyHa <laihuyha@gmail.com>
2022-03-13 22:04:36 +0700

    add require signin func

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express'); //importing express
 const router = express.Router(); //importing router
 
-const { signup, signin } = require('../Controllers/user'); //importing signup controller
+const { signup, signin, signout, requireSignin } = require('../Controllers/user'); //importing signup controller
 const { userSignupValidator } = require('../validator'); //importing signup validator
 
 // router.get('/', (req, res) => {
@@ -11,5 +11,10 @@ const { userSignupValidator } = require('../validator'); //importing signup vali
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
+
+//requireSignin example
+// router.get('/Hello', requireSignin, (req, res) => {
+//     res.send('Hello World! from node router');
+// });
 
 module.exports = router;
