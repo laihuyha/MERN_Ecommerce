e5b8b4718726611dfbb215f141c660335575818a
HuyHa <laihuyha@gmail.com>
2022-03-21 00:37:20 +0700

    Delete Product API and Read Product API : IDK seem like not working with me, how about the other? (Using Postman)

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express'); //importing express
 const router = express.Router(); //importing router
 
-const { create, productById, read } = require('../Controllers/product'); //importing product controller 
+const { create, productById, read, remove } = require('../Controllers/product'); //importing product controller 
 const { requireSignin, isAdmin, isAuth } = require('../Controllers/auth'); //importing auth controller
 const { userById } = require('../Controllers/user'); //importing user controller
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 
 router.param('userId', userById);
 router.param('product', productById);
