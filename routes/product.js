a27eb34af5a79136f158a34f460264cf8b84d88f
HuyHa <laihuyha@gmail.com>
2022-03-21 01:33:46 +0700

    update method - lesson 39

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,14 @@
 const express = require('express'); //importing express
 const router = express.Router(); //importing router
 
-const { create, productById, read, remove } = require('../Controllers/product'); //importing product controller 
+const { create, productById, read, remove, update } = require('../Controllers/product'); //importing product controller 
 const { requireSignin, isAdmin, isAuth } = require('../Controllers/auth'); //importing auth controller
 const { userById } = require('../Controllers/user'); //importing user controller
 
 router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update);
 
 router.param('userId', userById);
 router.param('product', productById);
