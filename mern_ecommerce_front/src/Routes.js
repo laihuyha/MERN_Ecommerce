51ca7c2e0595fc7e4827aba66269364c690a4566
HuyHa <laihuyha@gmail.com>
2022-03-28 22:14:59 +0700

    push again

---
+++ mern_ecommerce_front/src/Routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Menu from "./core/Menu";
+
+const Routes = () => (
+  <BrowserRouter>
+    <div>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={() => <h1>Home</h1>} />
+        <Route path="/signin" component={() => <h1>Sign In</h1>} />
+        <Route path="/signup" component={() => <h1>Sign Up</h1>} />
+      </Switch>
+    </div>
+    {/* <Menu />
+    <Switch>
+      <Route exact path="/" component={() => <h1>Home</h1>} />
+      <Route path="/signin" component={() => <h1>Sign In</h1>} />
+      <Route path="/signup" component={() => <h1>Sign Up</h1>} />
+    </Switch> */}
+  </BrowserRouter>
+);
