7a6f3795d80ea27de9b6c6b8457068c47d98ad76
HuyHa <laihuyha@gmail.com>
2022-03-28 22:24:04 +0700

    adsasdjlasjdla

--- mern_ecommerce_front/src/Routes.js
+++ mern_ecommerce_front/src/Routes.js
@@ -7,9 +7,9 @@ const Routes = () => (
     <div>
       <Menu />
       <Switch>
-        <Route exact path="/" component={() => <h1>Home</h1>} />
-        <Route path="/signin" component={() => <h1>Sign In</h1>} />
-        <Route path="/signup" component={() => <h1>Sign Up</h1>} />
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
       </Switch>
     </div>
     {/* <Menu />
