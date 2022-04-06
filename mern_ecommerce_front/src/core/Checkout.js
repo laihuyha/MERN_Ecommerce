3b8b2c4b5f3c37683ef9d57fb0f1ca9cabb2fab0
HuyHa <laihuyha@gmail.com>
2022-04-06 21:35:51 +0700

    route and create braintree controller front

--- mern_ecommerce_front/src/core/Checkout.js
+++ mern_ecommerce_front/src/core/Checkout.js
@@ -1,22 +1,123 @@
 import React, { useState, useEffect, Fragment } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore";
+import { processPayment, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  //#region State
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {}, // Braintree instance for DropIn to use to create payment method //instance includes the payment method nonce
+    address: "",
+  });
+  //#endregion
+
+  //#region Effect and Functions
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+  //#region use for Brain tree
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then((data) => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({ clientToken: data.clientToken });
+      }
+    });
+  };
+  const showDropIn = (clientToken) => (
+    <div onBlur={() => setData({ ...data, error: "" })}>
+      {data.clientToken !== null && products.length > 0 ? (
+        <div>
+          <div className="gorm-group mb-3">
+            <label className="text-muted">Address</label>
+            <textarea
+              // onChange={handleChange("address")}
+              className="form-control"
+              value={data.address}
+              placeholder="Type your address here..."
+            />
+          </div>
+          <DropIn
+            options={{
+              authorization: data.clientToken,
+            }}
+            onInstance={(instance) => (data.instance = instance)}
+          />
+          <button onClick={buy} className="btn btn-success btn-block">
+            Buy
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+  const buy = () => {
+    //send requestPayment to braintree (data.instance.requestPaymentMehtod())
+    let nonce;
+    let getNonce = data.instance
+      .requestPaymentMethod()
+      .then((data) => {
+        // console.log("data", data);
+        nonce = data.nonce;
+        // console.log("What's sent?", nonce, getTotalUSD(products)); //send nonce and total to backend
+        const paymentData = {
+          paymentMethodNonce: nonce,
+          // amount: getTotal(products),
+          amount: getTotalUSD(products),
+        };
+        processPayment(userId, token, paymentData)
+          .then((response) => {
+            setData({ ...data, success: response.success });
+            // console.log(response);
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => {
+        // console.log("error", error);
+        setData({ ...data, error: error.message });
+      });
+  };
+  //#endregion
+  const str = "&#60;" + "&#61;" + "&#62;";
+  const parser = new DOMParser();
+  const decodedString = parser.parseFromString(
+    `<!doctype html><body>${str}`,
+    "text/html"
+  ).body.textContent;
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
     }, 0);
   };
+  // const getTotalUSD = () => {
+  //   return products.reduce((currentValue, nextValue) => {
+  //     return parseFloat(
+  //       (currentValue + nextValue.count * nextValue.price) / 23000
+  //     ).toFixed(2);
+  //   }, 0);
+  // };
+
+  const getTotalUSD = () => {
+    return products.reduce((currentValue, nextValue) => {
+      const exchangeRate = Number(23000);
+      const price = Number(
+        (currentValue + nextValue.count * nextValue.price) / exchangeRate
+      ).toFixed(2);
+      return price;
+    }, 0);
+  };
   const showCheckout = () => {
     return isAuthenticated() ? (
       <div className="mt-7">
-        <button className="btn btn-success">
-          Check Out <i className="fas fa-chevron-right"></i>
-        </button>
+        <div>{showDropIn()}</div>
       </div>
     ) : (
       <div className="mt-7">
@@ -29,9 +130,27 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
       </div>
     );
   };
+  const showError = (error) => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+  const showSuccess = (success) => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      Thanks for buying!
+    </div>
+  );
+  //#endregion
+
   return (
     <Fragment>
-      <h2>
+      <h2 className="mb-2">
         Total :{" "}
         <span className="text-success">
           {getTotal().toLocaleString(navigator.language, {
@@ -39,7 +158,17 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
           })}{" "}
           VNĐ
         </span>
+        <span style={{ color: "Highlight" }}> {decodedString}</span>
+        <span className="text-success">
+          {" "}
+          {getTotalUSD().toLocaleString(navigator.language, {
+            minimumFractionDigits: 2,
+          })}{" "}
+          USD
+        </span>
       </h2>
+      {showError(data.error)}
+      {showSuccess(data.success)}
       {showCheckout()}
     </Fragment>
   );
---
+++ mern_ecommerce_front/src/core/showDetailsImage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { API } from "../config";
+
+//item = {product} and url means the url of the image
+const ShowImage = ({ item, url }) => {
+  return (
+    <div class="img-wrap">
+      <img
+        src={`${API}/${url}/photo/${item._id}`}
+        alt={item.name}
+        class="rounded-3 mt-1"
+        // style={{ height: "500px", width: "320px" }}
+      />
+      {/*url = product, item = product*/}
+    </div>
+  );
+};
+export default ShowImage;
