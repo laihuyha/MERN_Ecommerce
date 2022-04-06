b52428ec5a4dfc4e2fcb92bf670a3ff7aa7025fd
HuyHa <laihuyha@gmail.com>
2022-04-06 12:43:58 +0700

    add function to another button in the other pages

--- mern_ecommerce_front/src/core/CardRelate.js
+++ mern_ecommerce_front/src/core/CardRelate.js
@@ -1,19 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import "../assets/css/productCustom.css";
 import ShowImage from "./ShowImage";
 import moment from "moment";
+import { addItem, updateItem, removeItem } from "./cartHelpers";
 
 // import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
 // import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
 // import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 
-const CardRelate = ({ product, showViewProductButton = true }) => {
+const CardRelate = ({
+  product,
+  showViewProductButton = true,
+  showAddToCartButton = true,
+  cartUpdate = false,
+  showRemoveProductButton = false,
+  setRun = (f) => f, // default value of function
+  run = undefined, // default value of undefined
+}) => {
+  //#region state
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+  //#endregion
+
+  //#region useEffect and functions
   const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
         <Link to={`/product/${product._id}`} className="mr-2">
-          <button className="btn btn-sm btn-outline-primary me-1 float-right">
+          <button className="btn btn-sm btn-outline-primary me-1">
             View Product
           </button>
         </Link>
@@ -21,11 +36,28 @@ const CardRelate = ({ product, showViewProductButton = true }) => {
     );
   };
 
-  const showAddToCartButton = (showViewProductButton) => {
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const showAddToCart = (showAddToCartButton) => {
     return (
-      <button className="btn btn-sm btn-outline-warning me-1 float-right">
-        Add to Cart
-      </button>
+      showAddToCartButton && (
+        <button
+          onClick={addToCart}
+          className="btn btn-sm btn-outline-warning me-1"
+        >
+          Add to Cart
+        </button>
+      )
     );
   };
 
@@ -36,51 +68,127 @@ const CardRelate = ({ product, showViewProductButton = true }) => {
       <span className="badge badge-danger badge-fill">Out of Stock</span>
     );
   };
+
+  const handdleChange = (productId) => (event) => {
+    setRun(!run); // run useEffect in parent Cart
+    setCount(event.target.value < 1 ? 1 : event.target.value);
+    if (event.target.value >= 1) {
+      updateItem(productId, event.target.value);
+    }
+  };
+
+  const showCartUpdateOptions = (cartUpdate) => {
+    return (
+      cartUpdate && (
+        <Fragment>
+          <div className="input-group">
+            <input
+              type="number"
+              className="form-control"
+              value={count}
+              onChange={handdleChange(product._id)}
+            />
+          </div>
+        </Fragment>
+      )
+    );
+  };
+  const showCartRemoveButton = (showRemoveProductButton) => {
+    return (
+      showRemoveProductButton && (
+        <Fragment>
+          <button
+            className="btn btn-outline-danger"
+            onClick={() => {
+              removeItem(product._id);
+              setRun(!run); // run useEffect in parent Cart
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+              <path
+                fill-rule="evenodd"
+                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+              />
+            </svg>
+          </button>
+        </Fragment>
+      )
+    );
+  };
+  //#endregion
   return (
-    <figure class="card card-product">
-      {/* <div class="img-wrap">
+    <div>
+      {shouldRedirect(redirect)}
+      <figure class="card card-product" style={{ height: "600px" }}>
+        {/* <div class="img-wrap">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/5.webp" />
             </div> */}
-      <ShowImage item={product} url="product" />
-      <figcaption class="info-wrap">
-        <a href="/" class="title h3" name>
-          {product.name}
-        </a>
-        <p className="card-p  mt-2">
-          Short-Description : {product.description.substring(0, 50)}
-        </p>
-        <p className="lead mt-2">
-          {" "}
-          Category:{product.category && product.category.name}
-        </p>
-        <p className="lead mt-2">
-          {" "}
-          Add on:{moment(product.createdAt).fromNow()}
-        </p>
-        <div class="rating-wrap">
-          {/* <div class="label-rating">132 reviews</div> */}
-          <div class="label-rating">Sold : {product.sold}</div>
+        <ShowImage item={product} url="product" />
+        <figcaption class="info-wrap">
+          <a href="/" class="title h3" name>
+            {product.name}
+          </a>
+          <p className="card-p  mt-2">
+            Short-Description : {product.description.substring(0, 50)}
+          </p>
+          <p>
+            <span className="h5 mt-2 bg-sencondary">Category: </span>
+            <span>{product.category && product.category.name}</span>
+          </p>
+          <p>
+            <span className="h5 mt-2 bg-sencondary">Add on: </span>
+            <span>{moment(product.createdAt).fromNow()}</span>
+          </p>
+          <div class="rating-wrap">
+            {/* <div class="label-rating">132 reviews</div> */}
+            <div class="label-rating">
+              <span className="h6">Sold : </span>
+              <span>{product.sold}</span>
+            </div>
+          </div>
+        </figcaption>
+        <div class="bottom-wrap">
+          <div className="row">
+            <div className="col-md-6">
+              {showStock(product.quantity)}
+              <br></br>
+              <div class="price-wrap mt-10 h6">
+                <span class="price-new">
+                  <span className="text-success">Price : </span>
+                  {product.price.toLocaleString(navigator.language, {
+                    minimumFractionDigits: 0,
+                  })}{" "}
+                  VNĐ
+                </span>
+                {/* <del class="price-old">$1980</del> */}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div>
+                {showViewButton(showViewProductButton)}
+                {showAddToCart(showAddToCartButton)}
+              </div>
+              <div class="row mt-2">
+                <span className="col-sm-8">
+                  {showCartUpdateOptions(cartUpdate)}
+                </span>
+                <span className="col-sm-4">
+                  {showCartRemoveButton(showRemoveProductButton)}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </figcaption>
-      <div class="bottom-wrap">
-        {showStock(product.quantity)}
-        <br></br>
-        {showViewButton(showViewProductButton)}
-
-        {showAddToCartButton(showViewProductButton)}
-
-        <div class="price-wrap mt-3 h6">
-          <span class="price-new">
-            <span className="text-success">Price : </span>
-            {product.price.toLocaleString(navigator.language, {
-              minimumFractionDigits: 0,
-            })}{" "}
-            VNĐ
-          </span>
-          {/* <del class="price-old">$1980</del> */}
-        </div>
-      </div>
-    </figure>
+      </figure>
+    </div>
   );
 };
 export default CardRelate;
